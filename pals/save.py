a68569dec8396edccf572688ea106792586ff36a
fiter <vadym_prokopec@mail.ru>
2017-04-26 10:01:43 +0300

    Log files removed; Crawler modified

--- pals/save.py
+++ pals/save.py
@@ -1,32 +1,29 @@
-from pals.models import UserProfile
+from time import sleep
 import urllib.request
-import urllib3
+import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from time import sleep
-import re
+from pals.models import UserProfile
 
-b = webdriver.Chrome('/home/anon/Downloads/chromedriver')
+
+
+driver = webdriver.Chrome('/home/anon/Downloads/chromedriver')
 
 url = 'https://www.interpals.net/Thisiskittiya'
-b.get(url)
+driver.get(url)
 sleep(1)
-# soup = BeautifulSoup(html, 'html.parser')
-lst = b.find_elements_by_tag_name('h1')
+lst = driver.find_elements_by_tag_name('h1')
 username = lst[0].text
-lst = b.find_elements_by_tag_name('img')
+lst = driver.find_elements_by_tag_name('img')
 
-html = b.page_source
+html = driver.page_source
 country = re.search(r'<a href="country/\w+">(\w+(\s\w+){0,2})</a>', html).group(1)
 city = re.search(r'<a href="/app/search\?todo=search&amp;city=\d+">(\w+(\s\w+){0,2})</a>', html).group(1)
 state = city
 
-# city = lst[23].text
-# state = lst[24].text
+languages = driver.find_elements_by_class_name('prLangName')
 
-languages = b.find_elements_by_class_name('prLangName')
-
-languageLevels = b.find_elements_by_class_name('proflLevel')
+languageLevels = driver.find_elements_by_class_name('proflLevel')
 #Get level for all the languages
 pos=0
 for i in languageLevels:
@@ -37,8 +34,8 @@ for i in languageLevels:
 	pos+=1
 #languages is now a list of pairs - (Language, Level of Skill)
 
-pos=b.page_source.find('y.o.')
-age = b.page_source[pos-3:pos-1]
+pos=driver.page_source.find('y.o.')
+age = driver.page_source[pos-3:pos-1]
 
 soup = BeautifulSoup(html, 'html.parser')
 imageUrl = soup.find('img', {'height':'180', 'width':'180'})['src']
@@ -56,14 +53,14 @@ imageUrl = 'https:'+imageUrl
 # for (a, b) in languages:
 # 	print("%s Level: %s" %(a, b))
 
-q = UserProfile(username=username, 
+newUserProfile = UserProfile(username=username, 
 	city=city,
 	state=state,
 	country=country,
 	age = int(age),
 	imageUrl = imageUrl,
 	)
-q.save()
-b.close()
+newUserProfile.save()
+driver.close()
 
 # print(soup.prettify())
